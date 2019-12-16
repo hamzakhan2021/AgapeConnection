ec04547cc6d54f3469bb13b8a2f1b64d81f43417
vagrant <vagrant@homestead>
2019-12-16 08:01:57 +0000

    firstCommit

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register (Request $request) {
+
+        $validator = Validator::make($request->all(), [
+            'name'                  => 'required|string|max:255',
+            'email'                 => 'required|string|email|max:255|unique:users',
+            'password'              => 'required|string|min:6|confirmed',
+            'password_confirmation' => 'required',
+            'date_of_birth'         => 'required',
+            'phone_number'          => 'required|unique:users',
+            'gender'                => 'required',
+            'education_level'       => 'required',
+            'religion'              => 'required',
+        ]);
+
+        if ($validator->fails())
+        {
+            return response(['errors'=>$validator->errors()->all()], 422);
+        }
+        $request['password']=Hash::make($request['password']);
+        $user = User::create($request->toArray());
+
+        $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+        $response = [
+            'response'      => 1,
+            'message'       => 'User Sucessfully Registered',
+            'access_token'  => $token,
+            'data'          => $user
+            ];
+
+        return response($response, 200);
+    }
+
+    public function login (Request $request) {
+
+        $user = User::where('email', $request->email)->first();
+
+        if ($user) {
+
+            if (Hash::check($request->password, $user->password)) {
+                $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+                $response = [
+                    'response'      => 1,
+                    'message'       => 'Login Sucessfully',
+                    'access_token'  => $token,
+                    'data'          => $user
+                ];
+                return response($response, 200);
+            } else {
+                $response = [
+                    'response'      => 0,
+                    'message'       => 'Invalid Password',
+                ];
+                return response($response, 422);
+            }
+
+        } else {
+            $response = [
+                'response'      => 1,
+                'message'       => 'User does not exist',
+            ];
+            return response($response, 422);
+        }
+
+    }
+
+    public function logout (Request $request) {
+
+        $token = $request->user()->token();
+        $token->revoke();
+
+        $response = [
+            'response'      => 1,
+            'message'       => 'You have been succesfully logged out!',
+        ];
+        return response($response, 200);
+
+    }
+}
---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->integer('id')->autoIncrement();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->date('date_of_birth');
+            $table->string('phone_number')->unique();
+            $table->string('gender')->nullable();
+            $table->string('education_level')->nullable();;
+            $table->string('religion')->nullable();;
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->boolean('paid_account_status')->default(0);
+            $table->boolean('status')->default(0);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
